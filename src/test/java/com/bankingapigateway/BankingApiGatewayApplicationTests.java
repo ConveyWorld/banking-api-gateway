c6415727ed0f2f96c69be017ca0e1b4c438b514c
tan2203 <tranduytan.220300@gmail.com>
2025-04-14 22:15:09 +0700

    first commit

---
+++ src/test/java/com/bankingapigateway/BankingApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankingapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
